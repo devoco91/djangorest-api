39b806f7a72647aac98f5fd92abed47398d43271
David <abc@gmail.com>
2022-10-23 15:02:52 -1200

    first commit

---
+++ registration/serializers.py
@@ -0,0 +1,11 @@
+from .models import Applicants
+from rest_framework import serializers
+
+
+
+class ApplicantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Applicants
+        # fields= '__all__'
+        fields= ['id','firstname', 'lastname', 'email', 'phone','amount','address','course','center','mode']
+        
